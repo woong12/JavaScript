ab7e09c28f5e7aa992bec641a37ed626abcca966
woong <103757515+woong12@users.noreply.github.com>
2022-09-10 18:38:53 +0900

    Add files via upload

--- JavaScript/Chrome App/MOMENTUM/app.js
+++ JavaScript/Chrome App/MOMENTUM/app.js
@@ -1,1 +1,17 @@
-alert("hi");
+// alert("hi");
+
+const a = 5;
+const b = 2;
+let myName = "woong";
+
+/* const는 값을 업데이트 할 수 없음, let은 값을 업데이트 할 수 있음 */
+/* 기본적으로 const를 사용 */
+/* var은 되도록 사용 X */
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "woong2";
+
+console.log("your new name is " + myName);
