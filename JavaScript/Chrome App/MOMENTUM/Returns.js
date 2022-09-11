f33583e561a52114227795c7dee2bee743c2e36f
woong <103757515+woong12@users.noreply.github.com>
2022-09-11 20:41:31 +0900

    Add files via upload

---
+++ JavaScript/Chrome App/MOMENTUM/Returns.js
@@ -0,0 +1,33 @@
+// const age = 96;
+
+// function calculateKrAge(ageOfForeigner) {
+//     return ageOfForeigner + 2;
+// }
+
+// const krAge = calculateKrAge(age);
+
+// console.log(krAge);
+
+const calculator = {
+    plus: function (a, b) {
+        return a + b;
+    },
+    minus: function (a, b) {
+        return a - b;
+    },
+    divide: function (a, b) {
+        return a / b;
+    },
+    multiple: function (a, b) {
+        return a * b;
+    },
+    pow: function (a, b) {
+        return a ** b;
+    },
+};
+
+const plusResult = calculator.plus(2, 5);
+const minusResult = calculator.minus(plusResult, 5);
+const divideResult = calculator.divide(2, minusResult);
+const multipleResult = calculator.multiple(divideResult, plusResult);
+const powResult = calculator.pow(multipleResult, minusResult);
