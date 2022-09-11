efe160a12876309bc08db743c9357fab712e5236
woong <103757515+woong12@users.noreply.github.com>
2022-09-11 17:12:53 +0900

    Add files via upload

---
+++ JavaScript/Chrome App/MOMENTUM/assignment.js
@@ -0,0 +1,23 @@
+const calculator = {
+    plus: function (a, b) {
+        console.log(a + b);
+    },
+    minus: function (a, b) {
+        console.log(a - b);
+    },
+    divide: function (a, b) {
+        console.log(a / b);
+    },
+    multiple: function (a, b) {
+        console.log(a * b);
+    },
+    pow: function (a, b) {
+        console.log(a ** b);
+    },
+};
+
+calculator.plus(2, 5);
+calculator.minus(2, 5);
+calculator.divide(2, 5);
+calculator.multiple(2, 5);
+calculator.pow(2, 5);
