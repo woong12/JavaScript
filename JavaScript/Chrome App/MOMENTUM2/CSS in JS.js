07f8413f7c2cca7b51e442b9dee46eb78b4c3e7b
woong <103757515+woong12@users.noreply.github.com>
2022-09-13 04:41:18 +0900

    Add files via upload

--- JavaScript/Chrome App/MOMENTUM2/CSS in JS.js
+++ JavaScript/Chrome App/MOMENTUM2/CSS in JS.js
@@ -1,21 +1,32 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
-function handleMTitleClick() {
-    const currentColor = h1.style.color;
-    let newColor;
+function handleTitleClick() {
+    // const currentColor = h1.style.color;
+    // let newColor;
+
     // console.log(h1.style.color);
     // h1.style.color = "blue";
     // console.log(h1.style.color);
+    //2
+    // if (currentColor === "blue") {
+    //     newColor = "tomato";
+    // } else {
+    //     newColor = "blue";
+    // }
+    // h1.style.color = newColor;
 
-    if (currentColor === "blue") {
-        newColor = "tomato";
-    } else {
-        newColor = "blue";
-    }
-    h1.style.color = newColor;
+    const clickedClass = "clicked";
+
+    h1.classList.toggle("clicked");
+    //==
+    // if (h1.classList.contains(clickedClass)) {
+    //     h1.classList.remove(clickedClass);
+    // } else {
+    //     h1.classList.add(clickedClass);
+    // }
 }
 
-/*
+/* 2
 1) click event 발생 및 함수 실행
 2) currentColor 변수 선언 후 h1.style.color 값 복사 (getter)
 3) newColor 변수 선언
@@ -24,4 +35,23 @@ function handleMTitleClick() {
 6) 마지막으로 h1.style.color에 newColor값 대입 (setter)
 */
 
-h1.addEventListener("click", handleMTitleClick);
+h1.addEventListener("click", handleTitleClick);
+
+/*
+classList 우리가 class들의 목록으로 작업할수 있게끔 허용해준다.
+className은 이전calss를 상관하지않고 모든걸 교체해 버린다.
+
+classList를 이용하는건
+js에서 건드리는건 HTML element가 가지고있는 또하나의 요소 사용하는 것이다.
+= element의 class내용물을 조작하는 것을 허용한다는 뜻
+
+contains은 우리가 명시한 class가 HTML element의 class에 포함되어 있는지 말해준다
+
+toggle은 토큰이 존재하면 토큰제거
+토큰존재 하지않으면 토큰 추가
+
+ex)
+toggle은 h1의 classList에 clicked class가 이미있는지 확인하여
+만약있다면 toggle 이 clicked를 제거해준다
+만약 class name이 존재하지 않는다면 toggle은 classname 추가
+*/
