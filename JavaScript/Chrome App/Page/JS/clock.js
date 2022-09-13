e51fb19f38457d70a77fd786e33a8e09fe24de23
woong <103757515+woong12@users.noreply.github.com>
2022-09-13 23:25:27 +0900

    Add files via upload

--- JavaScript/Chrome App/Page/JS/clock.js
+++ JavaScript/Chrome App/Page/JS/clock.js
@@ -9,8 +9,12 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date();
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    //초의 자릿수가 1일때 앞에 0을 붙여주는 코드
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
-getClock(); //00:00:00이 출력되지 않게 즉시 호출
+getClock(); //00:00:00이 출력되지 않게 시간을 즉시 호출
 setInterval(getClock, 1000);
