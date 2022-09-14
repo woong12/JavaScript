a2138696c6475a90e089305ac2fc4af75554fb36
woong <103757515+woong12@users.noreply.github.com>
2022-09-14 23:50:24 +0900

    Add files via upload

--- JavaScript/Chrome App/Page/JS/todo.js
+++ JavaScript/Chrome App/Page/JS/todo.js
@@ -17,8 +17,9 @@ function deleteToDo(event) {
 
 function paintToDo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteToDo);
@@ -31,8 +32,12 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value; //input의 현재 value를 새로운 변수에 복사
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
