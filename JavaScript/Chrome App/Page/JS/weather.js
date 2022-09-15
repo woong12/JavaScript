ed236f0d802b04dbeb5d328d1f09078113e83640
woong <103757515+woong12@users.noreply.github.com>
2022-09-15 21:54:32 +0900

    Add files via upload

--- JavaScript/Chrome App/Page/JS/weather.js
+++ JavaScript/Chrome App/Page/JS/weather.js
@@ -1,7 +1,17 @@
+const API_KEY = "eff1c6eecb057d8ecc8b2c23aab12095";
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.latitude;
-    console.log("You live it", lat, lng);
+    // const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=126.9783&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
 }
 
 function onGeoError() {
