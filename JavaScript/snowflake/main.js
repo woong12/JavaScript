f54220888e0b18bbc098c4e246c8bd6c834de2cf
woong <103757515+woong12@users.noreply.github.com>
2022-12-25 23:39:55 +0900

    Update main.js

--- JavaScript/snowflake/main.js
+++ JavaScript/snowflake/main.js
@@ -1,27 +1,29 @@
-const body = document.querySelector("body");
+import "./styles.css";
+
 const MIN_DURATION = 10;
+
+const body = document.querySelector("body");
 
 function makeSnowflake() {
     const snowflake = document.createElement("div");
     const delay = Math.random() * 10;
-    const initialOpacity = Math.random();
     const duration = Math.random() * 20 + MIN_DURATION;
+    const initialOpacity = Math.random();
 
     snowflake.classList.add("snowflake");
+    snowflake.style.animation = `fall ${duration}s linear`;
+    snowflake.style.animationDelay = delay;
     snowflake.style.left = `${Math.random() * window.screen.width}px`;
-    snowflake.style.animationDelay = `${delay}s`;
     snowflake.style.opacity = initialOpacity;
-    snowflake.style.animationDuration = `fall ${duration}s linear`;
 
     body.appendChild(snowflake);
 
     setTimeout(() => {
         body.removeChild(snowflake);
         makeSnowflake();
-    }, (duration + delay) * 1000);
+    }, delay + duration * 1000);
 }
 
-makeSnowflake();
 for (let index = 0; index < 50; index++) {
-    setTimeout(makeSnowflake, 500 * index);
+    setTimeout(makeSnowflake, 200 * index);
 }
